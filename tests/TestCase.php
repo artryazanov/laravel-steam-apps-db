8ba0ccaf334a0cc5c7b80a49329732d791c6621f
Artem Ryazanov <artryazanov@gmail.com>
2026-02-02 19:12:44 +0700

    Add `$latestResponse` property to `TestCase` class for tracking test responses

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,9 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+
+    public static $latestResponse = null;
+
     use RefreshDatabase;
 
     /**
